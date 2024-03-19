4a5145701d0ac1475cc067127cc351f839b1c419
spamtactics <108055980+spamtactics@users.noreply.github.com>
2024-03-19 09:47:01 +0000

    Work in progress

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+
+
+
+public class Shooter extends SubsystemBase {
+  public VictorSPX leftShooter;
+  public VictorSPX rightShooter;
+  public Shooter(VictorSPX leftShooter, VictorSPX rightShooter){
+    this.leftShooter=leftShooter;
+    this.rightShooter=rightShooter;
+  }
+  public void shootAmp(){
+    //setting it to 50% power for now
+    leftShooter.set(ControlMode.PercentOutput, 0.1);
+    rightShooter.set(ControlMode.PercentOutput, 0.1);
+  }
+}
